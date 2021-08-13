8a84ae0b890bd02bc04b16072397f3de2b9d16fd
lucasdias38 <lucas_rodrigues3838@hotmail.com>
2021-08-13 19:26:16 -0300

    Atividade Calendar - Explicando atividade

--- src/main/java/atividades/AtividadeCalendar.java
+++ src/main/java/atividades/AtividadeCalendar.java
@@ -2,6 +2,15 @@ package atividades;
 import java.text.DateFormat;
 import java.util.*;
 
+/*                    Exercício Solicitado
+        - Um cliente tem 10 dias para pagar uma fatura após sua data de vencimento sem que
+            os juros sejam cobrados.
+        - Caso essa data caia em um sábado ou domingo, o cliente pode pagar na segunda-feira
+            seguinte.
+        - Crie uma estrutura que receba uma data de vencimento e calcule quantos dias ele tem
+            para pagar.
+* */
+
 public class AtividadeCalendar {
     public static void main(String[] args){
 
--- src/main/java/atividades/AtividadeDate.java
+++ src/main/java/atividades/AtividadeDate.java
@@ -4,7 +4,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
 /*                    Exercício Solicitado
         - Descruba o timelnMillis do dia que você nasceu;
         - Converta em um objeto Date;
