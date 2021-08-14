97a76b3a8373b9d3ade922ed7cd9bf0c45acdfd5
lucasdias38 <lucas_rodrigues3838@hotmail.com>
2021-08-14 10:47:46 -0300

    Atividade Date Format - Inserindo codigos

--- src/main/java/atividades/AtividadeDateFormat.java
+++ src/main/java/atividades/AtividadeDateFormat.java
@@ -1,4 +1,30 @@
 package atividades;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/*                    Exercício Solicitado
+        - Converta a Data atual no formato DD/MM/YYYY HH:MM:SS:MMM
+* */
+
 public class AtividadeDateFormat {
+    public static void main(String[] args){
+        Date dataAtual = new Date();
+            //Formatador
+        SimpleDateFormat dataHoraForm = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss:mmm");
+        SimpleDateFormat dataForm = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat horaForm = new SimpleDateFormat("hh:mm:ss:mmm");
+
+            //Formatando data e hr
+        String dataHoraFormatada = dataHoraForm.format(dataAtual);
+        String dataFormatada = dataForm.format(dataAtual);
+        String horaFormatado = horaForm.format(dataAtual);
+
+            // Saida de dados na tela
+        System.out.println("##############################################");
+        System.out.println("Data e Hora formatada em uma só variável.\n" + dataHoraFormatada);
+        System.out.println("Data formatada.\n" + dataFormatada);
+        System.out.println("Hora formatada.\n" + horaFormatado);
+        System.out.println("##############################################");
+    }
 }
